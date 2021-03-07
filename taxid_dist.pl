3668a324387c3e365b516cf9a0847435a6a9b6fb
Pombert-JF <jpombert@iit.edu>
2021-03-06 18:45:59 -0600

    fixed indents

--- taxid_dist.pl
+++ taxid_dist.pl
@@ -78,18 +78,18 @@ while (my $line = <NODES>){
 	my $rank = $columns[2];
 
 	### for (sort keys %ranks){ print "$_\n"; }
-    ###  Rank types:
-    # biotype, clade, class, cohort, family, forma, forma specialis, genotype, genus, infraclass, infraorder
-    # isolate, kingdom, morph, norank, order, parvorder, pathogroup, phylum, section, series, serogroup, serotype
-    # species, species group, species subgroup, strain, subclass, subcohort, subfamily, subgenus, subkingdom
-    # suborder, subphylum, subsection, subspecies, subtribe, subvariety, superclass, superfamily, superkingdom
-    # superorder, superphylum, tribe, varietas
+	###  Rank types:
+	# biotype, clade, class, cohort, family, forma, forma specialis, genotype, genus, infraclass, infraorder
+	# isolate, kingdom, morph, norank, order, parvorder, pathogroup, phylum, section, series, serogroup, serotype
+	# species, species group, species subgroup, strain, subclass, subcohort, subfamily, subgenus, subkingdom
+	# suborder, subphylum, subsection, subspecies, subtribe, subvariety, superclass, superfamily, superkingdom
+	# superorder, superphylum, tribe, varietas
 
-    if (exists $taxid{$txid}){
-	    $ranks{$rank}{$txid}[0] = $taxid{$txid};
-	    $ranks{$rank}{$txid}[1] = $parent_txid;
-    }
-    else{ print "TaxID $txid not found in DB!\n"; } ## Debugging line
+	if (exists $taxid{$txid}){
+		$ranks{$rank}{$txid}[0] = $taxid{$txid};
+		$ranks{$rank}{$txid}[1] = $parent_txid;
+	}
+	else{ print "TaxID $txid not found in DB!\n"; } ## Debugging line
 }
 
 ## Working on BLAST taxonomized outfmt6 files; treating each file as independent datasets
@@ -103,25 +103,21 @@ while (my $blast = shift@blast){
 	%bhits = (); %blasts = (); %counts = ();
 
 	while (my $line = <BLAST>){
-     
+
 		chomp $line;
 		my @col = split("\t", $line);
-        my $query = $col[0];    ## $col[0] = qseqid; $col[1] = sseqid; $col[2] = pident; $col[3] = length; $col[4] = bitscore; 
-        my $ev = $col[5];       ## $col[5] = evalue; $col[6] = staxids; $col[7] = sskingdoms; $col[8] = sscinames; $col[9] = sblastnames
-        $staxids = $col[6];
-        if ($staxids =~ /^(\d+);/){$staxids = $1;} ## Searching for multiple taxids, keeping only the 1st one
+		my $query = $col[0];    ## $col[0] = qseqid; $col[1] = sseqid; $col[2] = pident; $col[3] = length; $col[4] = bitscore; 
+		my $ev = $col[5];       ## $col[5] = evalue; $col[6] = staxids; $col[7] = sskingdoms; $col[8] = sscinames; $col[9] = sblastnames
+		$staxids = $col[6];
+		if ($staxids =~ /^(\d+);/){$staxids = $1;} ## Searching for multiple taxids, keeping only the 1st one
 
 		if ($ev <= $evalue){
 			if ((exists $bhits{$query}) && ($bhits{$query} >= $maxhits)){next;}
 			elsif ((exists $bhits{$query}) && ($bhits{$query} < $maxhits)){
 				$bhits{$query}++;
 				if (exists $ranks{'norank'}{$staxids}){$blasts{'norank'}{$staxids} += 1; $counts{'norank'}++;}
-				elsif (exists $ranks{'subspecies'}{$staxids}){
-                    subspecies();
-                }
-				elsif (exists $ranks{'species'}{$staxids}){
-                    species();
-                }
+				elsif (exists $ranks{'subspecies'}{$staxids}){ subspecies(); }
+				elsif (exists $ranks{'species'}{$staxids}){ species(); }
 				else {
 					print "WARNING: Taxonomy ID $staxids isn't a species, subspecies, or incertae sedis.";
 					if (exists $ranks{'strain'}{$staxids}){print " txid$staxids is a strain";}
@@ -132,12 +128,8 @@ while (my $blast = shift@blast){
 			else{
 				$bhits{$query} = 1;
 				if (exists $ranks{'norank'}{$staxids}){$blasts{'norank'}{$staxids} += 1; $counts{'norank'}++;}
-				elsif (exists $ranks{'subspecies'}{$staxids}){
-                    subspecies();
-                }
-				elsif (exists $ranks{'species'}{$staxids}){
-                    species();
-                }
+				elsif (exists $ranks{'subspecies'}{$staxids}){ subspecies(); }
+				elsif (exists $ranks{'species'}{$staxids}){ species(); }
 				else {
 					print "WARNING: Taxonomy ID $staxids isn't a species, subspecies, or incertae sedis.";
 					if (exists $ranks{'strain'}){print " txid$staxids is a strain";}
@@ -148,25 +140,25 @@ while (my $blast = shift@blast){
 		}
 	}
 	my $size;
-    my @ext = ('subspecies', 'species', 'genus', 'family', 'order', 'class', 'phylum', 'norank');
-    for my $ext (@ext){
-        if ($counts{$ext}){
-            open OUT, ">", "$blast.$ext";
-            print OUT "$ext\tTaxIF\tNumber\tPercent (total = $counts{$ext})\n";
-            foreach (sort {$blasts{$ext}{$b} <=> $blasts{$ext}{$a}} keys %{$blasts{$ext}}){
-                my $av = sprintf("%.2f%%", ($blasts{$ext}{$_}/$counts{$ext})*100);
-                print OUT "$taxid{$_}\t$_\t$blasts{$ext}{$_}\t$av\n";
-            }  
-        }
-        else {
-            print "No data found for rank: $ext\n";
-        }
-    }   
+	my @ext = ('subspecies', 'species', 'genus', 'family', 'order', 'class', 'phylum', 'norank');
+	for my $ext (@ext){
+		if ($counts{$ext}){
+			open OUT, ">", "$blast.$ext";
+			print OUT "$ext\tTaxIF\tNumber\tPercent (total = $counts{$ext})\n";
+			foreach (sort {$blasts{$ext}{$b} <=> $blasts{$ext}{$a}} keys %{$blasts{$ext}}){
+				my $av = sprintf("%.2f%%", ($blasts{$ext}{$_}/$counts{$ext})*100);
+				print OUT "$taxid{$_}\t$_\t$blasts{$ext}{$_}\t$av\n";
+			}
+		}
+		else {
+			print "No data found for rank: $ext\n";
+		}
+	}
 }
 
 ## Subroutines
 sub subspecies{
-    no warnings; ## Removing unnecessary verbosity is species is not defined.
+	no warnings; ## Removing unnecessary verbosity is species is not defined.
 	$blasts{'subspecies'}{$staxids} += 1; $counts{'subspecies'}++;
 	$blasts{'species'}{$ranks{'subspecies'}{$staxids}[1]} += 1; $counts{'species'}++;
 	$blasts{'bgenus'}{$ranks{'species'}{$ranks{'subspecies'}{$staxids}[1]}[1]} += 1; $counts{'genus'}++;
@@ -177,7 +169,7 @@ sub subspecies{
 }
 sub species{
 	no warnings; ## Removing unnecessary verbosity is species is not defined.
-    $blasts{'species'}{$staxids} += 1; $counts{'species'}++;
+	$blasts{'species'}{$staxids} += 1; $counts{'species'}++;
 	$blasts{'genus'}{$ranks{'species'}{$staxids}[1]} += 1; $counts{'genus'}++;
 	$blasts{'family'}{$ranks{'genus'}{$ranks{'species'}{$staxids}[1]}[1]} += 1; $counts{'family'}++;
 	$blasts{'order'}{$ranks{'family'}{$ranks{'genus'}{$ranks{'species'}{$staxids}[1]}[1]}[1]} += 1; $counts{'order'}++;
