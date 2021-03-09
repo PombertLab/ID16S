cfb6628fe9a03ded5a963db24a52878d0be99d3b
Pombert-JF <jpombert@iit.edu>
2021-03-09 11:47:29 -0600

    Added output and verbosity switches

--- taxid_dist.pl
+++ taxid_dist.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 ## Pombert Lab, 2018
 my $name = 'taxid_dist.pl';
-my $version = '0.5';
+my $version = '0.5a';
 my $updated = '09/03/2021';
 
 use strict; use warnings; use Getopt::Long qw(GetOptions);
@@ -25,7 +25,8 @@ COMMAND		${name} \\
 		  -a TaxDumps/names.dmp \\
 		  -b Examples/*.megablast \\
 		  -e 1e-75 \\
-		  -h 1
+		  -h 1 \\
+		  -o species genus family order class phylum
 
 OPTIONS:
 -n (--nodes)	NCBI nodes.dmp file 
@@ -33,6 +34,10 @@ OPTIONS:
 -b (--blast)	NCBI blast output file(s) in oufmt 6 format
 -e (--evalue)	evalue cutoff [Default: 1e-75]
 -h (--hits)	Number of BLAST hits to keep; top N hits [Default: 1]
+-o (--output)	Output files by taxonomic ranks [Default: species genus family]
+		# Possible taxonomic rank options are:
+		# subspecies strain species genus family order class phylum
+-v (--verbose)	Adds verbosity
 OPTIONS
 die "\n$options\n" unless @ARGV;
 
@@ -41,18 +46,22 @@ my $namedmp;
 my @blast = ();
 my $evalue = 1e-75;
 my $maxhits = 1;
+my @outputs = ('species', 'genus', 'family');
+my $verbose;
 GetOptions(
 	'n|nodes=s' => \$node,
 	'a|names=s' => \$namedmp,
 	'b|blast=s@{1,}' => \@blast,
 	'e|value=s' => \$evalue,
-	'h|hits=i' => \$maxhits
+	'h|hits=i' => \$maxhits,
+	'o|outputs=s@{1,}' => \@outputs,
+	'v|verbose' => \$verbose
 );
 
 ## Initializing taxids -> names database
 my %taxid;
 open NAMES, "<", "$namedmp" or die "Can't read file $namedmp $!\n";
-print "Initializing taxonomic IDs...\n";
+if ($verbose){ print "\nInitializing taxonomic IDs...\n"; }
 
 while (my $line = <NAMES>){
 	chomp $line; $line =~ s/\t\|//g;
@@ -67,7 +76,7 @@ while (my $line = <NAMES>){
 ### Initializing taxonomic databases
 my %ranks;
 open NODES, "<", "$node" or die "Can't read file $node $!\n";
-print "Initializing taxonomic databases...\n";
+if ($verbose){ print "Initializing taxonomic databases...\n"; }
 
 while (my $line = <NODES>){
 
@@ -77,10 +86,9 @@ while (my $line = <NODES>){
 	my $parent_txid = $columns[1];
 	my $rank = $columns[2];
 
-	### for (sort keys %ranks){ print "$_\n"; }
 	###  Rank types:
 	# biotype, clade, class, cohort, family, forma, forma specialis, genotype, genus, infraclass, infraorder
-	# isolate, kingdom, morph, norank, order, parvorder, pathogroup, phylum, section, series, serogroup, serotype
+	# isolate, kingdom, morph, no rank, order, parvorder, pathogroup, phylum, section, series, serogroup, serotype
 	# species, species group, species subgroup, strain, subclass, subcohort, subfamily, subgenus, subkingdom
 	# suborder, subphylum, subsection, subspecies, subtribe, subvariety, superclass, superfamily, superkingdom
 	# superorder, superphylum, tribe, varietas
@@ -88,8 +96,14 @@ while (my $line = <NODES>){
 	if (exists $taxid{$txid}){
 		$ranks{$rank}{$txid}[0] = $taxid{$txid};
 		$ranks{$rank}{$txid}[1] = $parent_txid;
+		$ranks{$rank}{$txid}[2] = $rank;
 	}
 	else{ print "TaxID $txid not found in DB!\n"; } ## Debugging line
+}
+if ($verbose){
+	print "\nFound taxonomic ranks:\n";
+	for (sort keys %ranks){ print "$_\n"; }
+	print "\n";
 }
 
 ## Working on BLAST taxonomized outfmt6 files; treating each file as independent datasets
@@ -97,8 +111,8 @@ while (my $line = <NODES>){
 my %bhits; my %blasts; my %counts; my $staxids;
 while (my $blast = shift@blast){
 
-	print "Parsing $blast...\n";
 	open BLAST, "<", "$blast" or die "Can't read file: $blast $!\n";
+	if ($verbose){ print "Parsing $blast...\n"; }
 
 	%bhits = (); %blasts = (); %counts = ();
 
@@ -134,14 +148,19 @@ while (my $blast = shift@blast){
 
 	## Working on output files
 	my $size;
-	my @ext = ('subspecies', 'strain', 'species', 'genus', 'family', 'order', 'class', 'phylum', 'norank');
+	my @ext = @outputs;
 	for my $ext (@ext){
 		if ($counts{$ext}){
-			open OUT, ">", "$blast.$ext";
-			print OUT "$ext\tTaxIF\tNumber\tPercent (total = $counts{$ext})\n";
-			foreach (sort {$blasts{$ext}{$b} <=> $blasts{$ext}{$a}} keys %{$blasts{$ext}}){
-				my $av = sprintf("%.2f%%", ($blasts{$ext}{$_}/$counts{$ext})*100);
-				print OUT "$taxid{$_}\t$_\t$blasts{$ext}{$_}\t$av\n";
+			open OUT, ">", "$blast.$ext" or die "Can't create file: $blast.$ext $!\n";
+			print OUT "$ext\tTaxID\tNumber\tPercent (total = $counts{$ext})\n";
+			foreach my $key (sort {$blasts{$ext}{$b} <=> $blasts{$ext}{$a}} keys %{$blasts{$ext}}){
+				
+				my $label; ## NOTE: the nodes.dmp sometimes includes taxid called 'no rank'
+				if (!defined $taxid{$key}){ $label = 'Undef rank'; }
+				else { $label = $taxid{$key}; }
+
+				my $av = sprintf("%.2f%%", ($blasts{$ext}{$key}/$counts{$ext})*100);
+				print OUT "$label\t$key\t$blasts{$ext}{$key}\t$av\n";
 			}
 		}
 		else { print "No data found for taxonomic rank: $ext\n"; }
@@ -149,6 +168,8 @@ while (my $blast = shift@blast){
 }
 
 ## Subroutines
+## NOTE: the nodes.dmp sometimes includes taxid called 'no rank'
+## Messes up the reconstruction based on parents. Must fix that...
 sub subspecies{ 
 	no warnings; ## Removing unnecessary verbosity is species is not defined.
 	## Autoincrement subspecies, then its parents (species, genus, family...) + autoincrementing them
