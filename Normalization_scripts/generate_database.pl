8c8429fa378836d5c45ec744a9d7f3d7b057bbf2
Alexander Thomas Julian <ajulian@hawk.iit.edu>
2022-03-16 21:06:48 -0500

    generate_database.pl reworked to aid visualization

--- Normalization_scripts/generate_database.pl
+++ Normalization_scripts/generate_database.pl
@@ -1,13 +1,14 @@
 #! /usr/bin/perl
 ##
 my $name = 'generate_database.pl';
-my $version = '0.4a';
-my $updated = '2022-02-18';
+my $version = '0.5';
+my $updated = '2022-03-16';
 
 use strict;
 use warnings;
 use Getopt::Long qw(GetOptions);
 use PerlIO::gzip;
+use File::Path qw(make_path);
 
 my $usage = << "EXIT";
 NAME		${name}
@@ -17,6 +18,10 @@ SYNOPSIS	This script creates a database of 16s ribosomal RNA count for a provide
 		selection of organisms
 
 COMMAND		${name} \\
+		  -d /media/FatCat_1/Databases/NCBI/Complete_Seq_RefSeq/ \\
+		  -na ID16S_DB/TaxDump/names.dmp \\
+		  -no ID16S_DB/TaxDump/nodes.dmp \\
+		  -o ./rRNA_16S_DBs
 
 
 OPTIONS
@@ -40,6 +45,10 @@ GetOptions(
 	"o|out=s" => \$outdir,
 );
 
+unless(-d $outdir){
+	make_path($outdir,{mode=>0755});
+}
+
 my @preferred_ranks = ("superkingdom", "phylum", "class", "order", "family", "genus", "species");
 
 ###############################################################################
@@ -55,65 +64,73 @@ while (my $line = <NAMES>){
 	$names{$taxid} = $org_name;
 }
 
+
 ###############################################################################
 ## Create a link between the taxid of the organism and the number of 16S copies
 ###############################################################################
 
-opendir(ORG_DIR,$org_dir) or die("Unable to open $org_dir: $!\n");
-open OUT, ">", "$outdir/raw_16S_data.dmp" or die("Unable to open file $outdir/raw_16S_data.dmp\n");
 
 my %orgs_rna_count;
 my $missing_org = 0;
 my $total_org = 0;
-while (my $org_file = readdir(ORG_DIR)){
-	unless(-d $org_file){
-		open ORG_FILE, "<:gzip", "$org_dir/$org_file";
-		my $rRNA_count = 0;
-		my $tax_id;
-		my $org_name;
-		my $header;
-		my $feature;
-		WHILE: while (my $line = <ORG_FILE>){
-			chomp($line);
-			## If there is a header for rRNA
-			if ($line =~ /\s{2,}rRNAs\D+(\d+)/){
-				$rRNA_count = $1;
-				$header = 1;
-			}
-			if ($line =~ /\s+\/db_xref="taxon:(\d+)"/){
-				$tax_id = $1;
-				$org_name = $names{$tax_id};
-				if($header){
-					print("$org_name\t$tax_id\t$rRNA_count\n");
-					last WHILE;
+unless(-f "$outdir/raw_16S_data.dmp"){
+	opendir(ORG_DIR,$org_dir) or die("Unable to open $org_dir: $!\n");
+	open OUT, ">", "$outdir/raw_16S_data.dmp" or die("Unable to open file $outdir/raw_16S_data.dmp: $!\n");
+	OUTER:while (my $org_file = readdir(ORG_DIR)){
+		unless(-d $org_file){
+			open ORG_FILE, "<:gzip", "$org_dir/$org_file";
+			my $rRNA_count = 0;
+			my $tax_id;
+			my $org_name = '';
+			my $header;
+			my $feature = '';
+			WHILE: while (my $line = <ORG_FILE>){
+				chomp($line);
+				## If there is a header for rRNA
+				if ($line =~ /\s{2,}rRNAs\D+(\d+)/){
+					$rRNA_count = $1;
+					$header = 1;
 				}
-			}
-			## If there is no header, parse the full file
-			if ($line =~ /^\s{1,5}(gene|rRNA|tRNA|CDS)/){
-				$feature = $1;
-			}
-			if ($feature eq "rRNA"){
-				if ($line =~ /\/product="(.+?)"*/){
-					my $rRNA_type = $1;
-					if($rRNA_type =~ /16S/){
-						$rRNA_count++;
+				if ($line =~ /\s+\/db_xref="taxon:(\d+)"/){
+					$tax_id = $1;
+				}
+				## If there is no header, parse the full file
+				if ($line =~ /^\s{1,5}(gene|rRNA|tRNA|CDS)/){
+					$feature = $1;
+				}
+				if ($feature eq "rRNA"){
+					if ($line =~ /\/product="(.+)"*/){
+						my $rRNA_type = $1;
+						if($rRNA_type =~ /16S/){
+							$rRNA_count++;
+						}
 					}
 				}
 			}
+			if($rRNA_count > 0){
+				$orgs_rna_count{$tax_id} = $rRNA_count;
+				print OUT $tax_id."\t".$rRNA_count."\n";
+			}
+			else{
+				print("No 16S rRNA found for $org_file with TaxID of $tax_id!\n");
+				$missing_org ++;
+			}
+			$total_org ++;
+			close ORG_FILE;
 		}
-		if($rRNA_count > 0){
-			$orgs_rna_count{$tax_id} = $rRNA_count;
-		}
-		else{
-			print("$tax_id is missing 16S rRNA!\n");
-			$missing_org ++;
-		}
-		$total_org ++;
-		close ORG_FILE;
+	}
+	close ORG_DIR;
+	print ("\nrRNA data missing for $missing_org organisms of $total_org organisms\n\n");
+	close OUT;
+}
+else{
+	open IN, "<", "$outdir/raw_16S_data.dmp" or die("Unable to open file $outdir/raw_16S_data.dmp: $!\n");
+	while(my $line = <IN>){
+		chomp($line);
+		my ($tax_id,$rRNA_count) = split("\t",$line);
+		$orgs_rna_count{$tax_id} = $rRNA_count;
 	}
 }
-close ORG_DIR;
-print ("\nrRNA data missing for $missing_org organisms of $total_org organisms\n\n");
 
 ###############################################################################
 ## Create a link between a child and parent in taxonomic tree
@@ -141,11 +158,6 @@ close NODE_FILE;
 ###############################################################################
 ## Store desirable taxonomic links in a file
 ###############################################################################
-
-if(-d $outdir){
-	system "rm -r $outdir";
-}
-mkdir($outdir,0755);
 
 open LINKED, ">", "$outdir/taxon_links.tsv" or die("Unable to write to $outdir/taxon_links.tsv: $!\n");
 
@@ -197,22 +209,26 @@ foreach my $org (keys(%orgs_rna_count)){
 	my $parent_rank;
 	my $base_org = $org;
 	WHILE:while(0==0){
+
 		## Proceed if the organism has a deeper lineage
-		if($linked_nodes{$linked_nodes{$org}[0]}[0]){
+		## Exit the loop if no deeper lineage found
+
+		unless($linked_nodes{$org}[0]){
+			last WHILE;
+		}
+		elsif($linked_nodes{$linked_nodes{$org}[0]}[0]){
 			$parent = $linked_nodes{$org}[0];
 			$parent_rank = $linked_nodes{$parent}[1];
 			push(@{$rRNA_by_level{$parent_rank}{$parent}},$orgs_rna_count{$base_org});
 			$org = $parent;
 		}
-		## Exit the loop if no deeper lineage found
 		else{
 			last WHILE;
-			exit;
 		}
 	}
 }
 
-my $header = "Organism Name\tTaxID\t# of Organisms\tMin # of Gene Copies\tMax # of Gene Copies\tMean # of Gene Copies\tMode # of Gene Copies\tMedian # of Gene Copies\tSTD of # of Gene Copies\n";
+my $header = "Organism Name\tTaxID\t# of rRNA Copies\n";
 my %filehandles;
 foreach my $rank (@preferred_ranks){
 	$filehandles{$rank} = "$outdir/$rank.db";
@@ -226,16 +242,9 @@ foreach my $rank (keys(%rRNA_by_level)){
 	foreach my $org (keys(%{$rRNA_by_level{$rank}})){
 		my $org_name = $names{$org};
 		my @rRNA_count = @{$rRNA_by_level{$rank}{$org}};
-		my $num_samples = scalar(@rRNA_count);
-		my $min = min(@rRNA_count);
-		my $max = max(@rRNA_count);
-		my $mean = mean(@rRNA_count);
-		my $median = median(@rRNA_count);
-		my $mode = mode(@rRNA_count);
-		my $std = std(@rRNA_count);
 		unless($printed_taxons{$org}){
 			open OUT, ">>", $filehandles{$rank} or die("Unable to open file $filehandles{$rank}: $!");
-			print OUT ("$org_name\t$org\t$num_samples\t$min\t$max\t$mean\t$median\t$mode\t$std\n");
+			print OUT ("$org_name\t$org\t".join(";",@rRNA_count)."\n");
 			close OUT;
 		}
 		$printed_taxons{$org} = 1;
@@ -245,17 +254,11 @@ foreach my $rank (keys(%rRNA_by_level)){
 		while($linked_nodes{$linked_nodes{$org}[0]}[0]){
 			$parent = $linked_nodes{$org}[0];
 			$parent_rank = $linked_nodes{$parent}[1];
+			my $parent_name = $names{$parent};
 			my @rRNA_count = @{$rRNA_by_level{$parent_rank}{$parent}};
-			$num_samples = scalar(@rRNA_count);
-			$mean = mean(@rRNA_count);
-			$min = min(@rRNA_count);
-			$max = max(@rRNA_count);
-			$median = median(@rRNA_count);
-			$mode = mode(@rRNA_count);
-			$std = std(@rRNA_count);
 			unless($printed_taxons{$parent}){
 				open OUT, ">>", $filehandles{$parent_rank} or die("Unable to open file $filehandles{$rank}: $!");
-				print OUT ("$parent\t$num_samples\t$min\t$max\t$mean\t$median\t$mode\t$std\n");
+				print OUT ($parent_name."\t".$parent."\t".join(";",@rRNA_count)."\n");
 				close OUT;
 			}
 			$printed_taxons{$parent} = 1;
@@ -276,104 +279,4 @@ sub desirable_rank {
 			return 1;
 		}
 	}
-}
-
-## Calculates the minimum number of 16S rRNA for the species
-sub min {
-	my @values = @_;
-	my $min_value;
-	foreach my $value (@values){
-		unless($min_value){
-			$min_value = $value;
-		}
-		if ($value < $min_value){
-			$min_value = $value;
-		}
-	}
-	return $min_value;
-}
-
-## Calculates the maximum number of 16S rRNA for the species
-sub max {
-	my @values = @_;
-	my $max_value;
-	foreach my $value (@values){
-		unless($max_value){
-			$max_value = $value;
-		}
-		if ($value > $max_value){
-			$max_value = $value;
-		}
-	}
-	return $max_value;
-}
-
-## Calculates the mean number of 16S rRNA for the species
-sub mean {
-	my @values = @_;
-	my $total = 0;
-	foreach my $value (@values){
-		$total += $value;
-	}
-	my $mean = sprintf('%.0f',$total/scalar(@values));
-	return $mean;
-}
-
-## Calculates the median number of 16S rRNA for the species
-sub median {
-	my @values = @_;
-	my @sorted = sort{$a <=> $b}(@values);
-	my $median;
-	if (scalar(@sorted) % 2 == 0){
-		my $a_index = scalar(@sorted)/2;
-		my $b_index = $a_index - 1;
-		$median = ($sorted[$a_index]+$sorted[$b_index])/2;
-		$median = sprintf('%.0f',$median);
-	}
-	else{
-		my $index = sprintf('%.0f',scalar(@sorted)/2);
-		$median = $sorted[$index];
-	}
-	return $median;
-}
-
-## Calculates the mode number of 16S rRNA for the species
-sub mode {
-	my @values = @_;
-	my %most;
-	foreach my $value (@values){
-		if($most{$value}){
-			$most{$value} += 1;
-		}
-		else{
-			$most{$value} = 1;
-		}
-	}
-	my $max = 0;
-	my $mode;
-	my @keys = sort({$most{$b} <=> $most{$a}}keys(%most));
-	my @vals = @most{@keys};
-	unless(scalar(@keys) == 1){
-		if($vals[0] == $vals[1]){
-			$mode = "N/A";
-		}
-		else{
-			$mode = $keys[0];
-		}
-	}
-	else{
-		$mode = $keys[0];
-	}
-	return $mode;
-}
-
-## Calculates the standard deviation in the number of 16S rRNA for the species
-sub std {
-	my @values = @_;
-	my $mean = mean(@values);
-	my $summation = 0;
-	foreach my $value (@values){
-		$summation += ($value - $mean)**2;
-	}
-	my $std = sprintf('%.3f',sqrt($summation/scalar(@values)));
 }
