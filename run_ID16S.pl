24f9992801785b7e7a997a337b7a39b20405da95
Jean-Francois Pombert <jpombert@iit.edu>
2022-03-24 14:05:41 -0500

    readme

--- run_ID16S.pl
+++ run_ID16S.pl
@@ -14,7 +14,7 @@ my $usage = <<"EXIT";
 NAME		${name}
 VERSION		${version}
 UPDATED		${updated}
-SYNOPSIS	
+SYNOPSIS	Master script that runs the ID16S pipeline on FASTA/FASTQ files
 
 USAGE		${name} \\
 		  -fq *.fastq.gz \\
@@ -102,21 +102,21 @@ my @output_directories = ($fasta_dir,$blast_dir,$nonnormal_dir,$normal_dir);
 
 my ($run_ID16S,$ID16S_dir) = fileparse($0);
 
-if(exists $ENV{"ID16S_DB"}){
+if (exists $ENV{"ID16S_DB"}){
 	$db = $ENV{"ID16S_DB"};
 }
-else{
-	unless($db){
+else {
+	unless ($db){
 		print STDERR ("\$ID16S_DB is not set as an enviroment variable and -d (--db) was not provided.\n");
 		print("To use run_ID16S.pl, please add \$ID16S_DB to the enviroment or specify path with -d (--db)\n");
 		exit;
 	}
 }
 
-if(exists $ENV{"ID16S_HOME"}){
+if (exists $ENV{"ID16S_HOME"}){
 	$outdir = $ENV{"ID16S_HOME"};
 }
-else{
+else {
 	unless($outdir){
 		print STDERR ("\$ID16S_HOME is not set as an enviroment variable and -o (--outdir) was not provided.\n");
 		print ("To use run_ID16S.pl, please add \$ID16S_HOME to the enviroment or specify path with -o (--outdir)\n");
@@ -131,74 +131,41 @@ foreach my $dirs (@output_directories){
 }
 
 ###################################################################################################
-## run fastq2fasta
+## run fastq2fasta.pl
 ###################################################################################################
 
-# COMMAND	fastq2fasta.pl \\
-# 			  -f *.fastq \\
-# 			  -o FASTA \\
-# 			  -h 50 \\
-# 			  -v
-
-# -f (--fastq)	FASTQ files to convert
-# -o (--outdir)	Output directory [Default: ./]
-# -h (--headcrop)	Remove the first X nucleotides from 5' ## Useful for nanopore data
-# -v (--verbose)	Adds verbosity
-
-if(@fasta){
+if (@fasta){
 	foreach my $file (@fasta){
-		system("cp $file $fasta_dir/$file");
+		system ("cp $file $fasta_dir/$file");
 	}
 }
 
-if(@fastq){
-	print("\nConverting FASTQ to FASTA with fastq2fasta.pl\n");
+if (@fastq){
+	print ("\nConverting FASTQ to FASTA with fastq2fasta.pl\n");
 
 	my $crop_5end = '';
 	if ($headcrop){
 		$crop_5end = "--headcrop $headcrop";
 	}
 
-	system("$ID16S_dir/Core_scripts/fastq2fasta.pl \\
+	system ("$ID16S_dir/Core_scripts/fastq2fasta.pl \\
 			  --fastq @fastq \\
 			  --outdir $fasta_dir \\
 			  --min_length $min_length \\
 			  $headcrop
 	");
 }
-else{
-	die("FASTQ or FASTA files are required by run_ID16S.pl\n");
+else {
+	die ("FASTQ or FASTA files are required by run_ID16S.pl\n");
 }
 
 
 ###################################################################################################
-## run megablast
+## run megablast.pl
 ###################################################################################################
 
-# USAGE		megablast.pl \\
-# 			  -k megablast \\
-# 			  -q Examples/sample_1.fasta \\
-# 			  -d NCBI_16S/16S_ribosomal_RNA \\
-# 			  -e 1e-05 \\
-# 			  -c 10 \\
-# 			  -t 10 \\
-# 			  -o MEGABLAST \\
-# 			  -v
-
-# OPTIONS:
-# -k (--task)	megablast, dc-megablast, blastn [default = megablast]
-# -q (--query)	fasta file(s) to be queried
-# -d (--db)	NCBI nucleotide database to query [default = 16S_ribosomal_RNA]
-# -e (--evalue)	1e-05, 1e-10 or other [default = 1e-05]
-# -c (--culling)	culling limit [default = 10]
-# -t (--threads)	CPUs to use [default = 10]
-# -o (--outdir)	Output directory [Default: ./]
-# -x (--taxids)	Restrict search to taxids from file ## one taxid per line
-# -n (--ntaxids)	Exclude from search taxids from file ## one taxid per line
-# -v (--verbose)	Adds verbosity
-
-print("\nRunning BLAST search on FASTA files with megablast.pl\n");
-system("$ID16S_dir/Core_scripts/megablast.pl \\
+print ("\nRunning BLAST search on FASTA files with megablast.pl\n");
+system ("$ID16S_dir/Core_scripts/megablast.pl \\
 		  --task $task \\
 		  --query $fasta_dir/*.fasta \\
 		  --evalue $p_evalue \\
@@ -208,33 +175,11 @@ system("$ID16S_dir/Core_scripts/megablast.pl \\
 ");
 
 ###################################################################################################
-## run taxid_dist
+## run taxid_dist.pl
 ###################################################################################################
 
-# COMMAND		taxid_dist.pl \\
-#				  -n TaxDumps/nodes.dmp \\
-#				  -a TaxDumps/names.dmp \\
-#				  -b MEGABLAST/*.megablast \\
-#				  -e 1e-75 \\
-#				  -h 1 \\
-#				  -o output_dir \\
-#				  -r species genus family order class phylum \\
-#				  -v
-
-# OPTIONS:
-# -n (--nodes)	NCBI nodes.dmp file 
-# -a (--names)	NCBI names.dmp
-# -b (--blast)	NCBI blast output file(s) in outfmt 6 format
-# -e (--evalue)	evalue cutoff [Default: 1e-75]
-# -h (--hits)	Number of BLAST hits to keep; top N hits [Default: 1]
-# -o (--outdir)	Output directory [Default: ./]
-# -r (--ranks)	Output files by taxonomic ranks [Default: species genus family order class]
-# 		# Possible taxonomic rank options are:
-# 		# subspecies strain species genus family order class phylum superkingdom 'no rank'
-# -v (--verbose)	Adds verbosity
-
-print("\nAcquiring TaxIDs for BLAST hits\n");
-system("$ID16S_dir/Core_scripts/taxid_dist.pl \\
+print ("\nAcquiring TaxIDs for BLAST hits\n");
+system ("$ID16S_dir/Core_scripts/taxid_dist.pl \\
 		  --blast $outdir/BLAST/*.$task \\
 		  --nodes $db/TaxDump/nodes.dmp \\
 		  --names $db/TaxDump/names.dmp \\
@@ -245,26 +190,14 @@ system("$ID16S_dir/Core_scripts/taxid_dist.pl \\
 ");
 
 ###################################################################################################
-## get_organism_statistics
+## get_organism_statistics.pl
 ###################################################################################################
-
-# USAGE		get_organism_statistics.pl \
-#			  -s Paul/SUMMARY/bc04_16S.fasta.megablast.genus \
-#			  -d ID16S/Complete-Assemblies_rRNA_16S_DBs/ \
-#			  -n ID16S/names.dmp \
-#			  -o .tsv
-
-# OPTIONS
-# -s (--sample)	File output from run_Taxonomized.pl
-# -d (--db)		Directory of databases created by create_database.pl
-# -n (--names)	NCBI's names.dmp file
-# -o (--output)	Output filename [Default = Normalized_Microbiome_Composition.tsv]
 
 opendir(NNORM,$nonnormal_dir) or die("Unable to open $nonnormal_dir: $!\n");
 
 foreach my $file (readdir(NNORM)){
-	unless(-d $file){
-		system("$ID16S_dir/Normalization_scripts/get_organism_statistics.pl \\
+	unless (-d $file){
+		system ("$ID16S_dir/Normalization_scripts/get_organism_statistics.pl \\
 				  --sample $nonnormal_dir/$file \\
 				  --db $db/Normalization_DB \\
 				  --name $db/TaxDump/names.dmp \\
